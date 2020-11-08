dade1e7415426f1468c3941195a1e78f8202f8ce
yossy0201 <yossy0201msk@gmail.com>
2020-11-08 18:42:49 +0900

    front-back

--- like.sql
+++ like.sql
@@ -1,5 +1,17 @@
-- 「FROM purchases」のあとにコードを追加し、nameカラムが「プリン」を含むデータを取得してください
+-- 「FROM purchases」のあとにコードを追加し、nameカラムが「プリン」を含むデータを取得してください
 
 SELECT *
 FROM purchases
 WHERE name LIKE "%プリン%";
+
+-- 「FROM purchases」のあとにコードを追加し、nameカラムが「プリン」で始まるデータを取得してください
+--前方一致
+SELECT *
+FROM purchases
+WHERE name LIKE "プリン%";
+
+-- 「FROM purchases」のあとにコードを追加し、nameカラムが「プリン」で終わるデータを取得してください
+--後方一致
+SELECT *
+FROM purchases
+WHERE name LIKE "%プリン"
