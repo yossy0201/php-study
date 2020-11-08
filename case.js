798105003490d2801ea6185a98d7d0544c434b14
yossy0201 <yossy0201msk@gmail.com>
2020-11-08 16:08:00 +0900

    case

---
+++ case.js
@@ -0,0 +1,18 @@
+const rank = 2;
+
+switch (rank) {
+  case 1:
+    console.log("金メダルです！");
+    break;
+
+  // rankの値が2のcaseを追加してください
+  case 2:
+    console.log("銀メダルです！");
+    break;
+  
+  // rankの値が3のcaseを追加してください
+  case 3:
+    console.log("銅メダルです！");
+    break;
+  
+}
