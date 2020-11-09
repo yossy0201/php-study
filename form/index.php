90a39026602fd68236331528ae619e8916a27922
yossy0201 <yossy0201msk@gmail.com>
2020-11-09 18:37:14 +0900

    form

--- form/index.php
+++ form/index.php
@@ -24,12 +24,35 @@
         <div class="form-item">名前</div>
         <input type="text" name="name">
 
+        <div class="form-item">年齢</div>
+        <select name="age">
+          <option value="未選択">選択してください</option>
+          <!-- for文を用いて6歳から100歳までをoptionで選べるようにしましょう -->
+          <?php 
+            for ($i = 6; $i <= 100; $i++) {
+              echo "<option value='{$i}'>{$i}</option>";
+            } 
+          ?>
+        </select>
+
+        <div class="form-item">お問い合わせの種類</div>
+        <?php 
+          $types = array('Progateに関するお問い合わせ', 'Progateに対する意見', '採用に関するお問い合わせ', '取材・メディア関連のお問い合わせ', '料金に関するお問い合わせ', 'その他');
+         ?>
+        <!-- この下にselectタグを書いていきましょう -->
+        <select name="category">
+          <option value="未選択">選択してください</option>
+            <?php
+              foreach ($types as $type) {
+              echo "<option value='{$type}'>{$type}</option>";
+              }
+            ?>
+        </select>
+       
         <div class="form-item">内容</div>
         <textarea name="body"></textarea>
-
-        <!-- この下に送信ボタンを作りましょう -->
-        <input type="submit" value="送信">
         
+        <input type="submit" value="送信">
       </form>
     </div>
   </div>
