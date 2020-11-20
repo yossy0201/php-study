364faa0a990110b97fb10fbea25476c9a3160a27
yossy0201 <yossy0201msk@gmail.com>
2020-11-20 23:14:23 +0900

    index

--- foodMenu.php/index.php
+++ foodMenu.php/index.php
@@ -23,10 +23,16 @@ require_once('menu.php');
             <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
             <?php if ($menu instanceof Drink): ?>
               <p class="menu-item-type"><?php echo $menu->getType() ?></p>
-            <!-- elseを追加してください -->
             <?php else: ?>
-              <p>辛さ: <?php echo $menu->getSpiciness()?></p>
-            
+              <!-- for文を用いて、spicinessプロパティの数だけ繰り返し処理を行ってください -->
+              <?php for ($i=0; $i<$menu->getSpiciness(); $i++): ?>
+                <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/chilli.png" 
+                class="icon-spiciness">
+              ?>
+              
+                <?php endfor ?>
+              
+              
             <?php endif ?>
             <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?>（税込）</p>
             <input type="text" value="0" name="<?php echo $menu->getName() ?>">
