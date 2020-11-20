055afc25a4b0281fee6799186c0be1c37ac8917c
yossy0201 <yossy0201msk@gmail.com>
2020-11-21 00:27:49 +0900

    index

--- foodMenu.php/index.php
+++ foodMenu.php/index.php
@@ -20,19 +20,16 @@ require_once('menu.php');
         <?php foreach ($menus as $menu): ?>
           <div class="menu-item">
             <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
-            <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
+            <h3 class="menu-item-name">
+              <!-- 以下の1行をaタグで囲んでください -->
+              <a href="show.php"><?php echo $menu->getName() ?></a>
+            </h3>
             <?php if ($menu instanceof Drink): ?>
               <p class="menu-item-type"><?php echo $menu->getType() ?></p>
             <?php else: ?>
-              <!-- for文を用いて、spicinessプロパティの数だけ繰り返し処理を行ってください -->
               <?php for ($i=0; $i<$menu->getSpiciness(); $i++): ?>
-                <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/chilli.png" 
-                class="icon-spiciness">
-              ?>
-              
-                <?php endfor ?>
-              
-              
+                <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/chilli.png" class='icon-spiciness'>
+              <?php endfor ?>
             <?php endif ?>
             <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?>（税込）</p>
             <input type="text" value="0" name="<?php echo $menu->getName() ?>">
