5dcebf47ea503bd897d87c5039645a42925dd3df
yossy0201 <yossy0201msk@gmail.com>
2020-11-20 23:00:44 +0900

    data

--- foodMenu.php/data.php
+++ foodMenu.php/data.php
@@ -1,11 +1,13 @@
 <?php
-require_once('menu.php');
+require_once('drink.php');
+require_once('food.php');
 
-// それぞれpriceとimageを追加してください
-$juice = new Menu('JUICE', 600, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png');
-$coffee = new Menu('COFFEE', 500, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png');
-$curry = new Menu('CURRY', 900, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png');
-$pasta = new Menu('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png');
+$juice = new Drink('JUICE', 600, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png', 'アイス');
+$coffee = new Drink('COFFEE', 500, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png', 'ホット');
+// $curryの引数に数値の3を追加してください
+$curry = new Food('CURRY', 900, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png', 3);
+// $pastaの引数に数値の1を追加してください
+$pasta = new Food('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png', 1);
 
 $menus = array($juice, $coffee, $curry, $pasta);
 
