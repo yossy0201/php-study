2be875d9c0fb95dab4543ff44ffce044cbcd6b48
yossy0201 <yossy0201msk@gmail.com>
2020-11-25 18:57:42 +0900

    upDate

---
+++ foodMenu.php/update.js
@@ -0,0 +1,8 @@
+const animals = ["dog", "cat", "sheep"];
+
+// 配列animalsの3つ目の要素を「rabbit」に更新してください
+console.log(animals[2]);
+animals[2] = "rabbit";
+
+// 配列animalsの3つ目の要素をコンソールに表示して下さい
+console.log(animals[2]);
