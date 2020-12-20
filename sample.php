771906662d1daf9d80b10c212d8c7be0461a3abb
yossy0201 <yossy0201msk@gmail.com>
2020-12-20 23:29:20 +0900

    sampleFiles

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>sample</title>
+</head>
+
+<body>
+    <h1>Hello world!</h1>
+    <?php echo "php test"; ?>
+</body>
+
+</html>
